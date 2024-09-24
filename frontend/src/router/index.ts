4dfe91c79317b2b4a75dd68f4cd678cdc8c8e404
Ilya Fedoseev <ilya.fedoseev@biglanguage.com>
2024-09-24 11:09:30 +0100

    if/fix-package-lock

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,15 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   component: About,
+  // },
+];
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-  ]
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router
