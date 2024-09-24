3f26736a2213ba3094d6f920afff736dc777692c
Ilya Fedoseev <ilya.fedoseev@biglanguage.com>
2024-09-24 22:53:09 +0100

    if/add-gallery-card

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,143 @@
+<script setup lang="ts">
+import { defineProps, computed, ref, type StyleValue } from 'vue';
+
+interface CardData {
+  img: string;
+  title: string;
+  text: string;
+}
+
+const props = defineProps<{
+  card: CardData;
+}>();
+
+const isHovered = ref(false); 
+
+const cardStyle = computed<StyleValue>(() => {
+  const backgroundImageUrl = `${props.card.img}`;
+
+  return {
+    backgroundImage: `url('${backgroundImageUrl}')`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    borderRadius: '10px',
+  };
+});
+</script>
+
+<template>
+  <div 
+    class="card"
+    :class="{'is-hovered': isHovered}"
+    :style="cardStyle"
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
+  >
+    <div class="overlay">
+      <h2 class="overlay__title">{{ card.title }}</h2>
+      <p class="overlay__text" :class="{'hidden': !isHovered}">{{ card.text }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  padding: 20px;
+  overflow: hidden;
+  border-radius: 10px;
+  transition: filter 0.3s ease;
+
+  h2, p {
+    z-index: 1;
+    transition: opacity 0.5s ease;
+  }
+
+  &.is-hovered {
+    h2 {
+      opacity: 1;
+    }
+    p {
+      opacity: 0.8;
+    }
+    .overlay {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  .hidden {
+    opacity: 0;
+    transform: translateY(100%);
+    transition: opacity 0.5s ease, transform 0.5s ease;
+  }
+
+  .overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    background: linear-gradient(
+      to top, 
+      rgba(17, 17, 17, 0.9),
+      rgba(164, 163, 163, 0.01)
+    );
+    transform: translateY(50%);
+    transition: opacity 0.5s ease, transform 0.5s ease;
+
+    &__title {
+      display: flex;
+      flex-direction: column;
+      margin-top: auto;
+      padding: 20px;
+    }
+
+    &__text {
+      padding: 20px;
+      transition: opacity 0.5s ease, transform 0.5s ease;
+    }
+  }
+
+  @media (max-width: 600px) {
+    width: 320px;
+    height: 320px;
+  }
+
+  @media (min-width: 601px) and (max-width: 1024px) {
+    width: 344px;
+    height: 344px;
+    .overlay {
+      transform: translateY(30%);  
+      &__title {
+        margin-top: 40%;
+      }
+    }
+  }
+
+  @media (min-width: 1025px) and (max-width: 1199px) {
+    width: 454px;
+    height: 454px;
+    .overlay {
+      transform: translateY(30%);  
+      &__title {
+        margin-top: 50%;
+      }
+    }
+  }
+
+  @media (min-width: 1200px) {
+    width: 344px;
+    height: 344px;
+    .overlay {
+      transform: translateY(30%);  
+      &__title {
+        margin-top: 40%;
+      }
+    }
+  }
+}
+</style>
---
+++ frontend/src/components/CardsGallery.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+// import { defineProps } from 'vue';
+import Card from './Card.vue';
+
+interface CardData {
+  img: string;
+  title: string;
+  text: string;
+}
+
+const cardsList: CardData[] = [
+  {
+    img: '/images/cpu_1.jpg',
+    title: 'Карточка 1',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+  {
+    img: '/images/cpu_2.jpg',
+    title: 'Карточка 2',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+  {
+    img: '/images/cpu_3.jpg',
+    title: 'Карточка 3',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+  {
+    img: '/images/cpu_1.jpg',
+    title: 'Карточка 4',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+  {
+    img: '/images/cpu_1.jpg',
+    title: 'Карточка 5',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+  {
+    img: '/images/cpu_1.jpg',
+    title: 'Карточка 6',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus alias rem minima itaque iure hic obcaecati repellendus eveniet quasi, libero nobis commodi dolorum, vel at pariatur ipsam minus? Dolore, in?'
+  },
+]
+
+// const props = defineProps<{
+//   cardsList: CardData[];
+// }>();
+</script>
+
+<template>
+  <div class="cards-container">
+    <div v-for="(card, index) in cardsList" :key="index">
+      <Card :card="card" />
+    </div>
+  </div>
+</template>
+
+
+<style>
+.cards-container {
+  display: grid;
+  grid-gap: 20px;
+}
+
+@media (max-width: 600px) {
+  .cards-container {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  }
+}
+
+@media (min-width: 601px) and (max-width: 1024px) {
+  .cards-container {
+    display: grid;
+    grid-auto-flow: column;
+    grid-template-columns: repeat(auto-fit, minmax(344px, 1fr));
+  }
+}
+
+@media (min-width: 1025px) and (max-width: 1199px) {
+  .cards-container {
+    justify-items: center;
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (min-width: 1200px) {
+  .cards-container {
+    justify-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+</style>
