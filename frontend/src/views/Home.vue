c4550d5e566512373968716cf61605cae1bb66b5
Ilya Fedoseev <ilya.fedoseev@biglanguage.com>
2024-10-01 20:05:21 +0100

    if/extract-the-pagination-line-into-a-separate-component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,30 @@
 import axios from 'axios';
 import { ref, onMounted, computed } from 'vue';
 import CardsGallery from '../components/CardsGallery.vue';
-import Header from "../components/Header.vue";
+import Header from '../components/Header.vue';
+import PaginationLine from '../components/PaginationLine.vue';
 
 const data = ref<string | null>(null);
-const currentValue = ref(0);
-const currentIndex = ref(0);
 
 const startImages = [
-  '../public/images/employee_1.jpg',
-  '../public/images/employee_2.jpg',
-  '../public/images/employee_3.jpg',
+  {
+    img: '../public/images/employee_1.jpg',
+    employeeTitle: 'Оператор CPU станка',
+    employeeDescription: 'Елена Петрова работает уже больше года на позиции оператор CPU станка',
+  },
+  {
+    img: '../public/images/employee_2.jpg',
+    employeeTitle: 'Инженер конструктор',
+    employeeDescription: 'Иван Иванов получил предложение на позицию инженера конструктора спустя месяц после завершения курса',
+  },
+  {
+    img: '../public/images/employee_3.jpg',
+    employeeTitle: 'Наладчик CPU станка',
+    employeeDescription: 'Андрей Сидоров после прохождения курса устроился наладчиком',
+  },
 ];
+const currentIndex = ref(0);
+const currentImage = ref(startImages[0].img);
 
 const getData = async () => {
   try {
@@ -26,57 +39,47 @@ const getData = async () => {
   }
 };
 
-const timer = () => {
-  setInterval(() => {
-    if (currentValue.value < 1000) {
-      currentValue.value += 1;
-    } else {
-      currentIndex.value >= startImages.length - 1 ? currentIndex.value = 0 : currentIndex.value += 1;
-      currentValue.value = 0;
-    }
-  }, 10);
-};
-
 const switchLine = (index: number) => {
   currentIndex.value = index;
-  currentValue.value = 0;
+};
+const changeImg = (currentIndex: number) => {
+  currentImage.value = startImages[currentIndex].img
 }
 
-const currentImage = computed(() => startImages[currentIndex.value]);
-
 onMounted(async () => {
-  await getData();  
-  timer();
+  await getData();
 });
 </script>
 
 <template>
   <div class="main">
-    <div 
-      class="start-screen"
-      :style="{ backgroundImage: `url(${currentImage})` }"
-    >
+    <div class="start-screen" :style="{ backgroundImage: `url(${currentImage})` }">
       <div>
         <Header></Header>
-        <h1 class="start-screen__text">{{'Получи свою \n профессию'}}</h1>
-      </div>
-      <div class="start-screen__pagination">
-        <div v-for="(startImage, index) in startImages" :key="index" class="item" @click="switchLine(index)">
-          <div class="pagination-line-container">
-            <div class="filled" :style="{ width: `${index < currentIndex ? 100 : (index === currentIndex ? (currentValue / 10) : 0)}%` }"></div>
-            <div class="remaining"></div>
-            <input
-              type="range"
-              min="0"
-              :max="1000"
-              disabled
-              :value="index < currentIndex ? 1000 : (index === currentIndex ? currentValue : 0)"
-              @input="index === currentIndex ? currentValue = +( $event.target as HTMLInputElement ).value : null"
-              class="pagination-line"
-            />
+        <div class="text-container">
+          <h1 class="text">Освой новую профессию вместе с PROCPU</h1>
+          <div class="banner">
+            <h2 class="banner__title">10 000 +</h2>
+            <span class="banner__text">Наших клиентов уже освоили профессию</span>
           </div>
         </div>
       </div>
+      <div class="employee">
+        <div class="employee__title">
+          <h3>{{ startImages[currentIndex].employeeTitle }}</h3>
+        </div>
+        <div class="employee__description">
+          {{ startImages[currentIndex].employeeDescription }}
+        </div>
+        <PaginationLine 
+          :data="startImages"
+          @switchLine="switchLine"
+          @changeImg="changeImg"/>
+      </div>
+      <PaginationLine
+        :data="startImages"
+        @switchLine="switchLine"
+        @changeImg="changeImg"/>
     </div>
     <div class="container">
       <div class="gallery-tabs"></div>
@@ -100,7 +103,7 @@ onMounted(async () => {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 0px 60px;
+  padding: 0px 20px 40px 40px;
   width: 100%;
   height: 80vh;
   background-size: cover;
@@ -108,31 +111,30 @@ onMounted(async () => {
   background-repeat: no-repeat;
   border-radius: 30px;
   margin-bottom: 2rem;
-  &__text {
-    width: 40%;
-    color: rgb(239, 91, 11);
-    background-color: black;
-    opacity: 0.6;
-  }
   &__pagination {
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
     gap: 5px;
     margin-bottom: 20px;
   }
 }
-
-.pagination-line-container {
-  position: relative;
-  width: 100%;
-  height: 5px;
+.text {
+  width: 200px;
+  color: rgb(255, 255, 255);
 }
 
-.pagination-line {
-  height: 5px;
-  appearance: none;
-  background: transparent;
+.employee {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  color: rgb(255, 255, 255);
+  &__title {
+    margin-bottom: 5px;
+  }
+  &__description {
+    font-family: 'Tahoma', sans-serif;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
 }
 
 .item {
@@ -147,35 +149,12 @@ onMounted(async () => {
   background-color: rgba(255, 255, 255, 0.5);
 }
 
-.pagination-line::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 0;
-  height: 0;
+.banner {
+  display: none;
 }
 
-.pagination-line::-moz-range-thumb {
-  width: 0;
-  height: 0;
-}
-
-.pagination-line::-ms-thumb {
-  width: 0;
-  height: 0;
-}
-
-.filled {
-  border-radius: 5px;
-  height: 5px;
-  background: white;
-  position: absolute;
-}
-
-.remaining {
-  border-radius: 5px;
-  width: 100%;
-  height: 5px;
-  background: rgba(255, 255, 255, 0.4);
+.hide-elem {
+  display: none;
 }
 
 .container {
@@ -197,16 +176,23 @@ onMounted(async () => {
   overflow-x: auto;
 }
 
-@media (max-width: 600px) {
-  .container {
-    margin: 0px 60px;
+@media (min-width: 100px) and (max-width: 600px){
+  .start-screen {
+    padding: 0 30px;
   }
 }
 
 @media (min-width: 601px) and (max-width: 1024px) {
+  .start-screen {
+    height: 90vh;
+    padding: 30px 60px;
+  }
 }
 
-@media (min-width: 1025px) and (max-width: 1199px) {
+@media (min-width: 1025px) and (max-width: 1279px) {
+  .text {
+    width: 40%;
+  }
   .gallery {
     max-width: 928px;
   }
@@ -216,14 +202,61 @@ onMounted(async () => {
   .container {
     margin: 0px 40px;
   }
+  .start-screen {
+    height: 90vh;
+    padding: 30px 135px;
+  }
 }
 
-@media (min-width: 1200px) {
+@media (min-width: 1280px) {
+  .banner {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(69, 69, 69);
+    width: 250px;
+    height: 130px;
+    border-radius: 20px;
+    border-width: 2px;
+    border-color: rgb(255, 255, 255);
+    transform: rotate(9deg);
+    opacity: 0.8;
+    padding: 20px;
+    cursor: pointer;
+    &__title {
+      color: rgb(214, 214, 214);
+    }
+    &__text {
+      font-family: 'Tahoma', sans-serif;
+      font-weight: 400;
+      font-size: 20px;
+      color: rgb(214, 214, 214);
+    }
+  }
+  .banner:hover {
+    background-color: rgb(107, 105, 105);
+  }
+  .banner:hover .banner__title {
+      color: rgb(255, 255, 255);
+  }
+
+  .banner:hover .banner__text {
+      color: rgb(255, 255, 255);
+  }
+  .text-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+  .text {
+    width: 60%;
+  }
   .gallery {
     max-width: 1072px;
   }
   .start-screen {
-    height: 90vh;
+    height: 95vh;
+    padding: 30px 135px;
   }
 }
 </style>
